d390a8b10c39dc348ba62c5b06dd5a83fc5a81b6
PhongTn <phongtn.group1@gmail.com>
2025-06-04 23:37:01 +0700

    add memory

--- devkit/agent_dev.py
+++ devkit/agent_dev.py
@@ -2,19 +2,29 @@ from textwrap import dedent
 from typing import Optional
 
 from agno.agent import Agent
+from agno.memory.v2 import Memory
+from agno.memory.v2.db.sqlite import SqliteMemoryDb
 from agno.models.base import Model
 from agno.storage.sqlite import SqliteStorage
 from agno.tools.file import FileTools
+from agno.tools.giphy import GiphyTools
 from agno.tools.googlesearch import GoogleSearchTools
 from agno.tools.python import PythonTools
 from agno.tools.reasoning import ReasoningTools
 from agno.tools.shell import ShellTools
 from .prompt import AGENT_DEV_DESCRIPTION, AGENT_DEV_INSTRUCTION
+from .tools.pexels import PexelsTools
+
 
 def load_local_storage():
     return SqliteStorage(
         table_name="agent_dev_sessions",
         db_file="../data.db"
+    )
+
+def load_agent_memory():
+    return Memory(
+        db=SqliteMemoryDb(table_name="memory", db_file="../data.db")
     )
 
 # docker_tools = DockerTools(
@@ -42,10 +52,14 @@ def build_agent(
         user_id=user_id,
         session_id=session_id,
         model=model,
+        memory=load_agent_memory(),
+        enable_agentic_memory=True,
         tools=[GoogleSearchTools(),
                ShellTools(),
                PythonTools(),
                FileTools(),
+               GiphyTools(),
+               PexelsTools(),
                # DockerTools(),
                ReasoningTools(think=True, analyze=True)],
         storage=load_local_storage(),
